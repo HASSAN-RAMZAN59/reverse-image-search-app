b69ab936709eaab66b83d51a33921a9ee6ef4a6c
HASSAN-RAMZAN59 <hasaanramzan59@gmail.com>
2026-07-13 12:23:17 +0500

    chore: remove watchFolders configuration from metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -11,7 +11,4 @@ defaultConfig.resolver.extraNodeModules = {
   'react-native-svg': path.resolve(projectRoot, 'node_modules', 'react-native-svg', 'lib', 'commonjs', 'index.js')
 };
 
-// Watch node_modules so Metro notices changes in linked packages.
-defaultConfig.watchFolders = [path.resolve(projectRoot, 'node_modules')];
-
 module.exports = defaultConfig;
